eea662722b25cbbf970c7454760a89e72390514c
OsmanliFarid <osmanliferid686@gmail.com>
2025-03-26 15:41:41 +0400

    task1

---
+++ task1/app.js
@@ -0,0 +1,13 @@
+/*Task-01
+Konsola doğum tarixini çıxarın.
+Adınız, cinsiniz və doğum tarixiniz olan user obyekti yaradın (name, gender, birthday).
+Konsola doğum tarixini çıxarın.
+ */
+
+const OBJ = {
+    name: "Eli",
+    gender: "k",
+    birthday: 24 + " aprel " + 2005,
+
+}
+console.log(OBJ.birthday);
